084695dafafb0bc3f3d73303ed4832dc83fa9272
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-10-23 16:25:21 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -185,3 +185,22 @@
 // })
 
 // console.log(newKyoto);
+
+/**
+ * 三項演算子
+ */
+
+//  ある条件？　　条件がtrueのとき: 条件がfalseのとき
+// const val1 = 1 < 0 ? 'trueです': 'falseです';
+// console.log(val1);
+
+// const num = 1300;
+
+// const forMattedNum = typeof num === 'number'? num.toLocaleString() : '数値を入力してください';
+// console.log(forMattedNum);
+
+// const checkSum = (num1,num2) => {
+//   return num1 + num2 > 100 ? '100を超えています' : '許容範囲内です。';
+// }
+
+// console.log(checkSum(100, 20));
